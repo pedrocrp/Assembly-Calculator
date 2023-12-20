9459ac44709a3e8a1faa7141c57dd1a7e265e268
lizcostato <lizcostato@gmail.com>
2023-12-20 08:04:06 -0300

    Agora a propria soma/subtracao pede os argumentos, antes de fazer a operação

--- src/soma.asm
+++ src/soma.asm
@@ -1,45 +1,88 @@
+section .data
+    msg_arg1 db "Argumento 1: ", 0
+    tam_msg_arg1 EQU $-msg_arg1
+
+    msg_arg2 db "Argumento 2: ", 0
+    tam_msg_arg2 EQU $-msg_arg2
+
 section .text
     global soma
+    extern scanf_16
+    extern scanf_32
+    extern string_para_int
+    extern printf
 
 ; Função de Soma
-; Argumentos: Dois números na pilha e um indicador de largura (0 para 16 bits, 1 para 32 bits)
+; Argumentos: indicador de precisao (0 para 16 bits, 1 para 32 bits)
 ; Retorna: Resultado da soma em EAX
+%define precisao [ebp+8]
 %define aux [ebp-4]
+%define arg1 [ebp-8]
+%define arg2 [ebp-12]
 soma:
-    enter 4,0
+    enter 12,0
 
     pusha
 
-    ; Verifica a largura dos operandos
-    movzx ecx, byte [ebp+8]  ; Indicador de largura (0 ou 1)
+    ; Verifica a precisao dos operandos
+    movzx ecx, byte precisao  ; Indicador de precisao (0 ou 1)
     cmp ecx, 0
     je soma_16bits
     jmp soma_32bits
 
 soma_16bits:
+
+    push tam_msg_arg1
+    push msg_arg1
+    call printf
+
+    call scanf_16
+    mov arg1, ax
+
+    push tam_msg_arg2
+    push msg_arg2
+    call printf
+
+    call scanf_16
+    mov arg2, ax
+
     ; Realiza a soma de 16 bits
-    movzx eax, word [ebp+12] ; Argumento 1
-    movzx ebx, word [ebp+16] ; Argumento 2
+    movzx eax, word arg1 ; Argumento 1
+    movzx ebx, word arg2 ; Argumento 2
     add ax, bx
-    jmp end_function_16          ; Pula para o final da função
+    jmp end_function_16   
 
 soma_32bits:
+    push tam_msg_arg1
+    push msg_arg1
+    call printf
+
+    call scanf_32
+    mov arg1, eax
+
+    push tam_msg_arg2
+    push msg_arg2
+    call printf
+
+    call scanf_32
+    mov arg2, eax
+
     ; Realiza a soma de 32 bits
-    mov eax, [ebp+12] ; Argumento 1
-    mov ebx, [ebp+16] ; Argumento 2
+    mov eax, arg1 ; Argumento 1
+    mov ebx, arg2 ; Argumento 2
     add eax, ebx
-    jmp end_function_32          ; Pula para o final da função
+    jmp end_function_32   
 
 end_function_16:
     mov aux, ax
     popa
     movzx ax, aux
     leave
-    ret                      ; Retorna para o endereço no topo da pilha
+    ret 4                
 
 end_function_32:
     mov aux, eax
     popa
     mov eax, aux
     leave
-    ret                      ; Retorna para o endereço no topo da pilha
+    ret 4        
--- src/subtracao.asm
+++ src/subtracao.asm
@@ -1,45 +1,87 @@
+section .data
+    msg_arg1 db "Argumento 1: ", 0
+    tam_msg_arg1 EQU $-msg_arg1
+
+    msg_arg2 db "Argumento 2: ", 0
+    tam_msg_arg2 EQU $-msg_arg2
+
 section .text
     global subtracao
+    extern scanf_16
+    extern scanf_32
+    extern string_para_int
+    extern printf
 
 ; Função de Subtração
-; Argumentos: Dois números na pilha e um indicador de largura (0 para 16 bits, 1 para 32 bits)
+; Argumentos: indicador de precisao (0 para 16 bits, 1 para 32 bits)
 ; Retorna: Resultado da subtração em EAX
+%define precisao [ebp+8]
 %define aux [ebp-4]
+%define arg1 [ebp-8]
+%define arg2 [ebp-12]
 subtracao:
-    enter 4,0
+    enter 12,0
 
     pusha
 
-    ; Verifica a largura dos operandos
-    movzx ecx, byte [ebp+8]  ; Indicador de largura (0 ou 1)
+    ; Verifica a precisao dos operandos
+    movzx ecx, byte precisao  ; Indicador de precisao (0 ou 1)
     cmp ecx, 0
     je subtracao_16bits
     jmp subtracao_32bits
 
 subtracao_16bits:
+    push tam_msg_arg1
+    push msg_arg1
+    call printf
+
+    call scanf_16
+    mov arg1, ax
+
+    push tam_msg_arg2
+    push msg_arg2
+    call printf
+
+    call scanf_16
+    mov arg2, ax
+
     ; Subtração de 16 bits com números assinados
-    movsx eax, word [ebp+12] ; Argumento 1
-    movsx ebx, word [ebp+16] ; Argumento 2
-    sub ax, bx               ; Subtrai BX de AX
-    jmp end_function_16         ; Pula para o final da função
+    movsx eax, word arg1 ; Argumento 1
+    movsx ebx, word arg2 ; Argumento 2
+    sub ax, bx           ; AX - BX
+    jmp end_function_16   
 
 subtracao_32bits:
+    push tam_msg_arg1
+    push msg_arg1
+    call printf
+
+    call scanf_32
+    mov arg1, eax
+
+    push tam_msg_arg2
+    push msg_arg2
+    call printf
+
+    call scanf_32
+    mov arg2, eax
+
     ; Realiza a subtração de 32 bits
-    mov eax, [ebp+12] ; Argumento 1
-    mov ebx, [ebp+16] ; Argumento 2
-    sub eax, ebx      ; Subtrai EBX de EAX
-    jmp end_function_32         ; Pula para o final da função
+    mov eax, arg1 ; Argumento 1
+    mov ebx, arg2 ; Argumento 2
+    sub eax, ebx      ; EAX - EBX
+    jmp end_function_32  
 
 end_function_16:
     mov aux, ax
     popa
     movzx ax, aux
     leave
-    ret 12                     ; Retorna para o endereço no topo da pilha
+    ret 12  
 
 end_function_32:
     mov aux, eax
     popa
     mov eax, aux
     leave
-    ret 12                     ; Retorna para o endereço no topo da pilha
+    ret 12   
